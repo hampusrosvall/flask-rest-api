b9f8132e1a3a8dcfabc5625d6bb1bf7d0dff0ad8
Hampus Rosvall <hampusrosvall@hr.lan>
2020-04-12 13:28:54 +0200

    Fixed run

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList 
 from resources.store import Store, StoreList
 
-from db import db 
-
 app = Flask(__name__)
 app.secret_key = 'hampus'
 api = Api(app)
@@ -17,10 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 jwt = JWT(app, authenticate, identify)
 
-@app.before_first_request
-def crete_tables(): 
-    db.create_all()
-
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
@@ -28,6 +22,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__': 
+    from db import db 
     db.init_app(app)
     app.run(port = 5000, debug = True)
 
